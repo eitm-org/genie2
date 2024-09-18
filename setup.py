c24182ae5ff7512390f5c49cf5b3e620e6729e62
mubale-eit <mubale@eit.org>
2024-09-18 18:20:29 +0000

    first attempt to make genie pip installable

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
       name='genie',
       version='0.0.1',
       description='de novo protein design through equivariantly diffusing oriented residue clouds',
-      packages=['genie', 'genie.*'],
+      packages=['genie'],
       install_requires=[
             'tqdm',
             'numpy',
@@ -13,5 +13,6 @@ setup(
             'wandb',
             'tensorboard',
             'pytorch_lightning',
+            'genie'
       ],
 )
