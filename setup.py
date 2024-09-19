56194e1758d17e7f09622a95b95d501b025bb5dd
mubale-eit <mubale@eit.org>
2024-09-19 20:32:12 +0000

    manually adding submodules test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
       name='genie',
       version='0.0.1',
       description='de novo protein design through equivariantly diffusing oriented residue clouds',
-      packages=find_packages(),
+      packages=['genie', 'genie/sampler'],
       install_requires=[
             'tqdm',
             'numpy',
